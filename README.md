0013a493881207be80ff2cddcb161d013b186748
arodriguezresultsiq <arodriguez@resultsiq.io>
2025-03-27 21:01:50 -0500

    [000] - Get And update api custom object  (#1)
    * Update README.md
    
    First commit to my Integrating With HubSpot I: Foundations practicum repository
    
    * Get and edit pets data
    
    * created get to homepage
    * created update page to edit form data
    * created post to update data previously filled in the form
    * updated style
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
+# Final overview
+
+https://github.com/user-attachments/assets/85c83f0c-1672-4bd8-b219-658e5064ffe9
+
+
 # Welcome to the Integrating With HubSpot I: Foundations Practicum
 
 This repository is for the Integrating With HubSpot I: Foundations course. This practicum is one of two requirements for receiving your Integrating With HubSpot I: Foundations certification. You must also take the exam and receive a passing grade (at least 75%).
 
 To read the full directions, please go to the [practicum instructions](https://app.hubspot.com/academy/l/tracks/1092124/1093824/5493?language=en).
 
-**Put your HubSpot developer test account custom objects URL link here:** https://app.hubspot.com/contacts/l/objects/${custom-obj-number}/views/all/list
+**Put your HubSpot developer test account custom objects URL link here:** 
+https://app.hubspot.com/contacts/49600504/objects/2-42445264/views/all/list
+
 
 ___
 ## Tips:
--- index.js
+++ index.js
@@ -1,71 +1,101 @@
+/**
+ * @fileoverview A simple Express application that integrates with the HubSpot API.
+ * Fetches custom object data (e.g., pet details) and allows updating them via a form.
+ * @module
+ */
+
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
 
+// Environment variable for HubSpot API authorization
+const privateAppAccess = process.env.PRIVATE_APP_ACCESS_TOKEN;
+
+// The custom object ID used in HubSpot CRM API
+const customObjectId = '2-42445264';
+
+// Base URL for HubSpot API
+const baseUrl = 'https://api.hubapi.com';
+
+// HTTP headers required for making API requests to HubSpot
+const headers = {
+    Authorization: `Bearer ${privateAppAccess}`,
+    'Content-Type': 'application/json'
+};
+
+// URL endpoint for fetching and updating the custom object
+const url = `${baseUrl}/crm/v3/objects/${customObjectId}/?limit=100`;
+
+// Port for the Express server to listen on
+const port = process.env.PORT || 3000;
+
+// Setting up Express view engine (Pug)
 app.set('view engine', 'pug');
+
+// Middleware to serve static files and parse request bodies
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
-
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
+/**
+ * @function
+ * @name getPets
+ * @description Fetches pet data from HubSpot CRM using the HubSpot API and renders it in a homepage.
+ * @param {Request} req - The Express request object.
+ * @param {Response} res - The Express response object.
+ */
+app.get('/', async (req, res) => {
+    const params = {
+        properties: 'name,type,bio'  // Properties to fetch
+    };
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        // Fetching data from the HubSpot API
+        const response = await axios.get(url, { params, headers });
+        const pets = response.data.results;
+
+        // Rendering homepage with the fetched pet data
+        res.render('homepage', { pets });
     } catch (error) {
         console.error(error);
+        res.send('Error fetching data');
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
+/**
+ * @function
+ * @name updatePetForm
+ * @description Renders the update custom object form for updating pet details.
+ * @param {Request} req - The Express request object.
+ * @param {Response} res - The Express response object.
+ */
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+});
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+/**
+ * @function
+ * @name updatePetDetails
+ * @description Receives updated pet data from the form and posts it to HubSpot API.
+ * @param {Request} req - The Express request object.
+ * @param {Response} res - The Express response object.
+ */
+app.post('/update-cobj', async (req, res) => {
+    const data = {
+        properties: req.body  // Data from the form
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    try {
+        // Posting the updated data to HubSpot API
+        await axios.post(url, data, { headers });
+        res.redirect('/');  // Redirecting to the homepage after update
+    } catch (error) {
+        console.error(error);
+        res.send('Error updating data');
     }
-
 });
-*/
 
-
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+// Starting the Express server
+app.listen(port, () => {
+    console.log(`'Listening on http://localhost:${port}`);
+});
